e66f42c48651bd07b9c846a373621728ba53ab2b
Aditya Sethi <sethiadi8@gmail.com>
2018-09-09 18:18:21 +0530

    final commit

--- SimpleLCDDisplay.ino
+++ SimpleLCDDisplay.ino
@@ -3,18 +3,28 @@
 LiquidCrystal lcd(7, 8, 9, 10, 11 , 12);
 
 void setup() { 
+  lcd.begin(16, 2);
   lcd.setCursor(0,1);
+  lcd.write("Number: ");
 }
 
 void loop() {
+  //No connected sensor to test analogRead(A0)
   //int sensorValue = analogRead(A0);
+  
+  
 
   
   //int sensorValue = 250;
   
+  int i = 0;
   for(; i<999; i++)
   {
+    lcd.setCursor(9,1);
     lcd.print(i);
+    delay(100);
   }
   
 }
+
+//tested works final commit done
